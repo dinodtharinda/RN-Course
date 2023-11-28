7c067e8789476137a0c6209046d4643691f3e0e5
dinod tharinda <dinodtharinda2001@gmail.com>
2023-11-28 14:07:00 +0530

    initial commit

--- App.js
+++ App.js
@@ -3,31 +3,13 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.appContainer}>
+    <View >
       <StatusBar style="auto"/>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="ADD GOAL" />
-      </View>
-      <View>
-        <Text>List of Goals..</Text>
-      </View>
+      <Text>hello world!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent:'space-between'
-  },
-  textInput :{
-    borderWidth:1,
-    borderColor:'#cccccc',
-    width:'80%',
-    padding:8
-  }
+
 });
